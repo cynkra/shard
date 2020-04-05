6d393840bd911cee7ee1d1577492fbe7e517d0c3
Kirill Müller <krlmlr+r@mailbox.org>
2020-04-05 05:58:59 +0200

    Rename

--- R/csv.R
+++ R/csv.R
@@ -1,11 +1,11 @@
 #' @export
-write_csv_sharded <- function(x, name, dir, ..., shard_by = NULL, delimiter = "-") {
-  spec <- shard_split(x, name, "csv", shard_by = !!enexpr(shard_by), delimiter = delimiter)
-  spec$path <- file.path(dir, spec$path)
-  write_csv_spec(spec, ...)
+shard_write_csv <- function(x, name, dir, ..., shard_by = NULL, delimiter = "-") {
+  split <- shard_split(x, name, "csv", shard_by = !!enexpr(shard_by), delimiter = delimiter)
+  split$path <- file.path(dir, split$path)
+  write_csv_split(split, ...)
 }
 
-write_csv_spec <- function(spec, ...) {
-  fs::dir_create(unique(dirname(spec$path)))
-  pwalk(spec, function(data, path) readr::write_csv(data, path, ...))
+write_csv_split <- function(split, ...) {
+  fs::dir_create(unique(dirname(split$path)))
+  pwalk(split, function(data, path) readr::write_csv(data, path, ...))
 }
