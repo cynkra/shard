9bdbb1ac18d055562e21bf49d1371ccc6b1ab055
Kirill Müller <krlmlr+r@mailbox.org>
2020-04-05 06:12:20 +0200

    shard_read_csv()

--- R/csv.R
+++ R/csv.R
@@ -1,11 +1,20 @@
 #' @export
-shard_write_csv <- function(x, name, dir, ..., shard_by = NULL, delimiter = "-") {
+shard_write_csv <- function(x, name, dir = ".", ..., shard_by = NULL, delimiter = "-", na = "") {
   split <- shard_split(x, name, "csv", shard_by = !!enexpr(shard_by), delimiter = delimiter)
   split$path <- file.path(dir, split$path)
-  write_csv_split(split, ...)
+  write_csv_split(split, ..., na = na)
 }
 
 write_csv_split <- function(split, ...) {
   fs::dir_create(unique(dirname(split$path)))
   pwalk(split, function(data, path) readr::write_csv(data, path, ...))
+}
+
+#' @export
+shard_read_csv <- function(name, dir = ".", ..., delimiter = "-") {
+  # FIXME: Add spec = NULL argument, call readr::spec_csv() once if unset
+  path <- fs::dir_ls(fs::path(dir, name), glob = "*.csv", recurse = TRUE, type = c("file", "symlink"))
+  data <- map(path, readr::read_csv, ...)
+  split <- tibble(path = fs::path_rel(path, dir), data)
+  shard_bind(split, delimiter = delimiter)
 }
---
+++ tests/testthat/test-csv.R
@@ -0,0 +1,12 @@
+test_that("integration", {
+  dir <- tempfile("shard")
+
+  shard_write_csv(iris, "iris", dir, shard_by = c(Species, Sepal.Width))
+  iris_out <- shard_read_csv("iris", dir)
+
+  iris_chr <-
+    iris %>%
+    mutate_if(is.factor, as.character)
+
+  expect_df_equal(iris_out, iris_chr)
+})
