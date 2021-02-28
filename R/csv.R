007ee80cd13efcdb04fe96712149573be5c3322c
Kirill Müller <krlmlr+r@mailbox.org>
2021-02-28 07:48:55 +0100

    - `shard_read_csv()` and `shard_read_tsv()` gain `col_names` and `col_types` arguments, column specification is read only once for better performance.

--- R/csv.R
+++ R/csv.R
@@ -10,10 +10,25 @@ write_csv_split <- function(split, ...) {
 }
 
 #' @export
-shard_read_csv <- function(name, dir = ".", ..., delimiter = "-") {
-  # FIXME: Add spec = NULL argument, call readr::spec_csv() once if unset
-  path <- fs::dir_ls(fs::path(dir, name), glob = "*.csv", recurse = TRUE, type = c("file", "symlink"))
-  data <- map(path, readr::read_csv, ...)
+shard_read_csv <- function(name, dir = ".", ..., delimiter = "-", col_names = NULL, col_types = NULL, skip = NULL) {
+  stopifnot(is.null(skip))
+
+  path <- fs::dir_ls(fs::path_abs(name, start = dir), glob = "*.csv", recurse = TRUE, type = c("file", "symlink"))
+
+  if (length(path) == 0) {
+    return(tibble())
+  }
+
+  if (is.null(col_names) || is.null(col_types)) {
+    if (is.null(col_names)) {
+      col_names <- TRUE
+    }
+
+    suppressMessages(col_types <- readr::spec_csv(path[[1]], col_names = col_names, col_types = col_types))
+    col_names <- names(col_types$cols)
+  }
+
+  data <- map(path, readr::read_csv, ..., col_names = col_names, col_types = col_types, skip = 1L)
   split <- tibble(path = fs::path_rel(path, dir), data)
   shard_bind(split, delimiter = delimiter)
 }
--- R/tsv.R
+++ R/tsv.R
@@ -10,10 +10,25 @@ write_tsv_split <- function(split, ...) {
 }
 
 #' @export
-shard_read_tsv <- function(name, dir = ".", ..., delimiter = "-") {
-  # FIXME: Add spec = NULL argument, call readr::spec_tsv() once if unset
-  path <- fs::dir_ls(fs::path(dir, name), glob = "*.tsv", recurse = TRUE, type = c("file", "symlink"))
-  data <- map(path, readr::read_tsv, ...)
+shard_read_tsv <- function(name, dir = ".", ..., delimiter = "-", col_names = NULL, col_types = NULL, skip = NULL) {
+  stopifnot(is.null(skip))
+
+  path <- fs::dir_ls(fs::path_abs(name, start = dir), glob = "*.tsv", recurse = TRUE, type = c("file", "symlink"))
+
+  if (length(path) == 0) {
+    return(tibble())
+  }
+
+  if (is.null(col_names) || is.null(col_types)) {
+    if (is.null(col_names)) {
+      col_names <- TRUE
+    }
+
+    suppressMessages(col_types <- readr::spec_tsv(path[[1]], col_names = col_names, col_types = col_types))
+    col_names <- names(col_types$cols)
+  }
+
+  data <- map(path, readr::read_tsv, ..., col_names = col_names, col_types = col_types, skip = 1L)
   split <- tibble(path = fs::path_rel(path, dir), data)
   shard_bind(split, delimiter = delimiter)
 }
